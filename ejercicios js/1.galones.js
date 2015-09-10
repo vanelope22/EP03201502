c136e26607de696b0f689d06eff191f0d5c0012a
vanelope22 <bvsco22@gmail.com>
2015-09-10 17:45:57 -0500

    Update 1.galones.js

--- ejercicios js/1.galones.js
+++ ejercicios js/1.galones.js
@@ -1,4 +1,4 @@
 
 var g= prompt("ingrese la cantidad de galones a calcular :");
-var l=(galones/3.7854); 
-alert("la cantidad de galones es igual a "+litros+" Lt");
+var l=(g/3.7854); 
+alert("la cantidad de galones es igual a "+l+" Lt");
